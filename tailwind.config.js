455f59ced3eed7ef219847bd437c683bebc8b4a6
Kayrat <makhmudov.kay@gmail.com>
2024-10-08 23:47:38 +0500

    mark-up header, banner and solutions

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'open': ['"Open Sans"', 'Helvetica', 'Arial', 'sans-serif'],
+      'oswald': ['Oswald', 'Helvetica', 'Arial', 'sans-serif'],
+    },
+    extend: {
+      boxShadow: {
+        'bottom': '0 0 11px 2px rgba(0, 0, 0, 0.1)',
+      }
+    },
   },
   plugins: [],
 }
