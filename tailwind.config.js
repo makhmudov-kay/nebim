607f91e607ddbd7d305d579917d7ed3fe530e49c
Kayrat <makhmudov.kay@gmail.com>
2024-10-09 23:37:54 +0500

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
         'bottom': '0 0 11px 2px rgba(0, 0, 0, 0.1)',
       }
     },
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1170px",
+    },
   },
   plugins: [],
 }
