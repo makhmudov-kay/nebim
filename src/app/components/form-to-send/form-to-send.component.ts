22258a505af073f07ee27836214e77bc6eb51ea3
Kayrat <makhmudov.kay@gmail.com>
2024-10-27 21:36:24 +0500

    .

--- src/app/components/form-to-send/form-to-send.component.ts
+++ src/app/components/form-to-send/form-to-send.component.ts
@@ -1,9 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { TranslateModule } from '@ngx-translate/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { TelegramService } from '../../services/telegram.service';
+import { finalize } from 'rxjs';
+import {
+  NzNotificationModule,
+  NzNotificationService,
+} from 'ng-zorro-antd/notification';
 
 @Component({
   selector: 'app-form-to-send',
@@ -16,10 +34,64 @@ import { TranslateModule } from '@ngx-translate/core';
     NzButtonModule,
     NzIconModule,
     TranslateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzNotificationModule,
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormToSendComponent implements OnInit {
-  constructor() {}
+  form!: FormGroup;
+  isLoading = false;
 
-  ngOnInit() {}
+  constructor(
+    private fb: FormBuilder,
+    private telegram$: TelegramService,
+    private $cd: ChangeDetectorRef,
+    private notification: NzNotificationService
+  ) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      phone: [null, [Validators.required]],
+      fullName: [null, [Validators.required]],
+      message: [null, [Validators.required]],
+    });
+  }
+
+  createNotification(type: string): void {
+    this.notification.create(
+      type,
+      'Notification Title',
+      'This is the content of the notification. This is the content of the notification. This is the content of the notification.'
+    );
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    const request = this.form.getRawValue();
+    this.telegram$
+      .sendMessage(request.fullName, request.phone, request.message)
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+        })
+      )
+      .subscribe({
+        next: (res) => {
+          this.form.reset();
+          this.createNotification('success');
+        },
+        error: (err) => {
+          this.createNotification('error');
+        },
+        complete: () => {
+          this.isLoading = false;
+          this.$cd.markForCheck();
+        },
+      });
+  }
 }
---
+++ src/app/services/telegram.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TelegramService {
+  private token: string = '7776806797:AAE_VGVPeql19j2twCFBM4QAFCPaaP6ddqs'; // Ваш токен от BotFather
+  private chatId: string = '864805518'; // Ваш chat_id или id группы
+
+  constructor(private http: HttpClient) {}
+
+  sendMessage(name: string, phone: string, message: string): Observable<any> {
+    const url = `https://api.telegram.org/bot${this.token}/sendMessage`;
+    const body = {
+      chat_id: this.chatId,
+      text: `📩 Yangi murojaat❗️\n👤 Mijoz: ${name}\n📞 Raqam: <code>+${phone}</code>\n📄 Xabar: <code>${message}</code>`,
+      parse_mode: 'HTML',
+    };
+    return this.http.post(url, body);
+  }
+}
