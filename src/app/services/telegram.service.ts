cec37cc3079d1aaf194606c0cab975eb5f132665
Kayrat <makhmudov.kay@gmail.com>
2024-10-30 16:17:38 +0500

    .

--- src/app/services/telegram.service.ts
+++ src/app/services/telegram.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class TelegramService {
   private token: string = '7776806797:AAE_VGVPeql19j2twCFBM4QAFCPaaP6ddqs'; // Ваш токен от BotFather
-  private chatId: string = '864805518'; // Ваш chat_id или id группы
+  private chatId: string = '-4567305500'; // Ваш chat_id или id группы
 
   constructor(private http: HttpClient) {}
 
