50c948ac1764b651e2864efe00dbb51e7446a975
Kayrat <makhmudov.kay@gmail.com>
2024-10-30 15:20:57 +0500

    .

--- src/app/components/solutions/solutions.component.ts
+++ src/app/components/solutions/solutions.component.ts
@@ -14,26 +14,32 @@ export class SolutionsComponent {
     {
       title: 'solution.title-1',
       text: 'solution.text-1',
+      img: './assets/images/security.svg',
     },
     {
       title: 'solution.title-2',
       text: 'solution.text-2',
+      img: './assets/images/finance.svg',
     },
     {
       title: 'solution.title-3',
       text: 'solution.text-3',
+      img: './assets/images/planning.svg',
     },
     {
       title: 'solution.title-4',
       text: 'solution.text-4',
+      img: './assets/images/crm.svg',
     },
     {
       title: 'solution.title-5',
       text: 'solution.text-5',
+      img: './assets/images/produce.svg',
     },
     {
       title: 'solution.title-6',
       text: 'solution.text-6',
+      img: './assets/images/lead-management.svg',
     },
   ];
 }
